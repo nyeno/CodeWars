cae572053d9c07c4b8e7233226b56377763a48f1
Eno Nyenooke Laura <nyenooke.eno@stu.cu.edu.ng>
2022-05-18 15:59:05 +0100

    Create grasshopperSummation.js
     a program that finds the summation of every number from 1 to num

---
+++ grasshopperSummation.js
@@ -0,0 +1,10 @@
+var summation = function (num) {
+  // Code here
+  let counter = num
+  let output = 1
+  while(counter !== 1){
+       output = output + counter
+    counter --
+}
+  return output
+}
