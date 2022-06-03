9b06bb63fcece2100512f7f2aeffe1fc3ad9a5e9
Eno Nyenooke Laura <nyenooke.eno@stu.cu.edu.ng>
2022-06-03 23:03:30 +0100

    Update restSpread.js

--- restSpread.js
+++ restSpread.js
@@ -2,6 +2,7 @@ function shuffleIt(arr,...a){
   //coding here...
   
   for(let i=0; i<a.length; i++){
+    // use destructuring to switch values without using a temp variable
     [arr[a[i][0]], arr[a[i][1]]] = [arr[a[i][1]], arr[a[i][0]]] 
   }
   return arr
