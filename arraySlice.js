8cd6ce1a912d50d2f9c8d2ca6054569371cb0ad6
Eno Nyenooke Laura <nyenooke.eno@stu.cu.edu.ng>
2022-06-10 22:03:08 +0100

    Update and rename arraySlice to arraySlice.js

--- arraySlice.js
+++ arraySlice.js
@@ -1,6 +1,7 @@
 function threeInOne(arr){
   //coding here...
   var output = []
+  //use slice to create a duplicate array
   var newArr = arr.slice()
   while(newArr.length!=0){
     let cut = newArr.splice(0,3)
