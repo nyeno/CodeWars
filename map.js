e149a8f5c9a97a6a006ec3004584891aba9c8eba
Eno Nyenooke Laura <nyenooke.eno@stu.cu.edu.ng>
2022-06-13 18:10:54 +0100

    Update map.js

--- map.js
+++ map.js
@@ -2,10 +2,12 @@ function isolateIt(arr){
   //coding here...
   let b = arr.map(x => {
     let a = x.split('')
+    //check for even letter words
     if(a.length%2 ===0){
       a.splice(a.length/2, 0, "|")
     }
     else{
+      //check for odd letter words
       a[a.length/2 - 0.5] = "|"
   }
      return a.join('')
