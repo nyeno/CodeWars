fd4da833e197f98cbf57b0f3299f1b9d15bca257
Eno Nyenooke Laura <nyenooke.eno@stu.cu.edu.ng>
2022-06-16 10:59:43 +0100

    Create some.js

---
+++ some.js
@@ -0,0 +1,26 @@
+function mirrorImage(arr){
+  //coding here...
+  let a= ""
+  let b = ""
+  let c = ""
+  let output = []
+    
+  let d = arr.some((x,i) => {
+    if(i + 1 < arr.length){
+      a = arr[i].toString()
+      b = arr[i+1].toString().split('').reverse().join('')
+      c = arr[i+1].toString()
+     
+    }  
+  
+    return a == b
+  })
+  if(d){
+     output.push(Number(a), Number(c))
+  }
+ else{
+   output.push(-1, -1)
+ }
+ return output
+
+}
