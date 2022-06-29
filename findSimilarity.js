56733ff597fefdd8dffdfe2c4730131bd2a32435
Eno Nyenooke Laura <nyenooke.eno@stu.cu.edu.ng>
2022-06-29 17:57:05 +0100

    Update findSimilarity.js

--- findSimilarity.js
+++ findSimilarity.js
@@ -1,8 +1,8 @@
 function findSimilarity(str,word){
   //coding here...
-  var regstr=word[0]                             //first letter
-          +word.slice(1,-1).replace(/./g,".")  //middle letters
-          +word.slice(-1);                     //last letter
+  var regstr=word[0]                             
+          +word.slice(1,-1).replace(/./g,".")  
+          +word.slice(-1);                    
   var reg1=new RegExp("^"+regstr+"$");
   return str.split(" ").filter(s => reg1.test(s)).join(" ")
 }
