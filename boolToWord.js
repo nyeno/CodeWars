9f4db4f7b2aaad41bf0db2dbd4861841ca082915
Eno Nyenooke Laura <nyenooke.eno@stu.cu.edu.ng>
2022-07-06 17:11:24 +0100

    Create boolToWord.js

---
+++ boolToWord.js
@@ -0,0 +1,11 @@
+function boolToWord( bool ){
+  //...
+  if(bool){
+    return "Yes"
+  }
+  else{
+    return "No"
+  }
+}
+
+//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
